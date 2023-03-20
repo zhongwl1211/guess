a8e1510f14c9149174d75156216b3be41d924ea9
Zhongwl1211 <fses90153@gmail.com>
2023-03-20 11:13:51 +0800

    third

--- guess.py
+++ guess.py
@@ -2,8 +2,12 @@
 #讓使用者重複輸入數字去猜
 #猜對了,印出"你答對了"
 #猜錯了,要告訴他比答案大還是小
+
+
 import random
-r = random.randint(1,100)
+start =int( input('請使用者輸入開始值:'))
+end = int(input('請使用者輸入結束值:'))
+r = random.randint(start,end)
 count = 0
 while True:
 	count = count + 1
