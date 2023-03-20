da14a6b370114a8e77fb3a233969fd546fc08da1
Zhongwl1211 <fses90153@gmail.com>
2023-03-20 11:01:40 +0800

    basic

---
+++ guess.py
@@ -0,0 +1,15 @@
+#產生一個隨機整數1~100
+#讓使用者重複輸入數字去猜
+#猜對了,印出"你答對了"
+#猜錯了,要告訴他比答案大還是小
+import random
+r = random.randint(1,100)
+while True:
+	num = int(input('請輸入數字:'))
+	if num == r:
+		print('你答對了')
+		break
+	elif num > r:
+		print('比答案大')	
+	elif num < r:
+		print('比答案小')
